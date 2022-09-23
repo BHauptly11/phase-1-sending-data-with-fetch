743c0333ede19e07f73e86689391ed7bffb8793d
Bartholomew Hauptly <bhauptly11@gmail.com>
2022-09-23 14:52:35 -0500

    commit tree be9ae218391d187268ec3e622fb6079f1aaf53f3

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-// Add your code here
+//const configurationObject = {
+  //  method: "POST",
+    //headers: {
+      //  "Content-Type": "application/json",
+        //"Accept": "application/json",
+    //},
+    //body: JSON.stringify({
+      //  dogName: "Byron",
+       // dogBreed: "Poodle",
+  //  }),
+//};
+
+//fetch("http://localhost:3000/dogs", configurationObject);
+
+//learn test
+//submitData = (name, email) => {
+ 
+
+    //return fetch("http://localhost:3000/users", configurationObject)
+    //.then(function(response) {
+    //    return response.json(body);  
+   // })
+   // .then(function (object){
+    //    return (object);
+        
+  //  })
+   // .catch(function (error) {
+    //    console.log(error.message);
+  //  });
+//}
+
+//const configurationObject = {
+  //  method: "POST",
+    //headers: {
+      //  "Content-Type": "application/json",
+      //  "Accept": "application/json",
+   // },
+    //body: JSON.stringify({
+      //  name: name,
+       // email: email,
+    //})
+//}   
+
+
+
+function submitData(name,email){
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+
+        })
+    
+    .then(function(response){
+    return response.json();
+    })
+})
+}
